7cb4eee3df4f89b4fc20d386fb7bbe681cf965e5
nchavchanidze <nikolozchavchanidze@gmail.com>
2023-02-10 13:11:58 +0400

    Update Link Icon

--- components/Title/Title.tsx
+++ components/Title/Title.tsx
@@ -18,7 +18,64 @@ const Title: React.FC<Props> = ({ title, url, company }) => {
                             target="_blank"
                             rel="noreferrer"
                         >
-                            {title} - {company} &#129141;
+                            {title} - {company}{" "}
+                            <svg
+                                viewBox="0 0 24 24"
+                                version="1.1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="#fff"
+                            >
+                                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                                <g
+                                    id="SVGRepo_tracerCarrier"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                ></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <g
+                                        id="Page-1"
+                                        stroke="none"
+                                        strokeWidth="1"
+                                        fill="none"
+                                        fill-rule="evenodd"
+                                    >
+                                        <g id="External-Link">
+                                            <rect
+                                                id="Rectangle"
+                                                fill-rule="nonzero"
+                                                x="0"
+                                                y="0"
+                                                width="24"
+                                                height="24"
+                                            ></rect>
+                                            <path
+                                                d="M20,12 L20,18 C20,19.1046 19.1046,20 18,20 L6,20 C4.89543,20 4,19.1046 4,18 L4,6 C4,4.89543 4.89543,4 6,4 L12,4"
+                                                id="Path"
+                                                stroke="#0C0310"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                            ></path>
+                                            <path
+                                                d="M16,4 L19,4 C19.5523,4 20,4.44772 20,5 L20,8"
+                                                id="Path"
+                                                stroke="#0C0310"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                            ></path>
+                                            <line
+                                                x1="11"
+                                                y1="13"
+                                                x2="19"
+                                                y2="5"
+                                                id="Path"
+                                                stroke="#0C0310"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                            ></line>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
                         </a>
                     );
                 } else if (url) {
@@ -29,7 +86,63 @@ const Title: React.FC<Props> = ({ title, url, company }) => {
                             target="_blank"
                             rel="noreferrer"
                         >
-                            {title} &#129141;
+                            {title} <svg
+                                viewBox="0 0 24 24"
+                                version="1.1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="#fff"
+                            >
+                                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                                <g
+                                    id="SVGRepo_tracerCarrier"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                ></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <g
+                                        id="Page-1"
+                                        stroke="none"
+                                        strokeWidth="1"
+                                        fill="none"
+                                        fill-rule="evenodd"
+                                    >
+                                        <g id="External-Link">
+                                            <rect
+                                                id="Rectangle"
+                                                fill-rule="nonzero"
+                                                x="0"
+                                                y="0"
+                                                width="24"
+                                                height="24"
+                                            ></rect>
+                                            <path
+                                                d="M20,12 L20,18 C20,19.1046 19.1046,20 18,20 L6,20 C4.89543,20 4,19.1046 4,18 L4,6 C4,4.89543 4.89543,4 6,4 L12,4"
+                                                id="Path"
+                                                stroke="#0C0310"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                            ></path>
+                                            <path
+                                                d="M16,4 L19,4 C19.5523,4 20,4.44772 20,5 L20,8"
+                                                id="Path"
+                                                stroke="#0C0310"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                            ></path>
+                                            <line
+                                                x1="11"
+                                                y1="13"
+                                                x2="19"
+                                                y2="5"
+                                                id="Path"
+                                                stroke="#0C0310"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                            ></line>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
                         </a>
                     );
                 } else if (company) {
