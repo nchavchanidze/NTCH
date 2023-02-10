331609be0423afc0351333773174f6e7643e12d5
nchavchanidze <nikolozchavchanidze@gmail.com>
2023-02-10 18:38:57 +0400

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import "@/styles/styles.scss";
 import Header from "@/components/Header/Header";
 
@@ -9,20 +8,6 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <Script
-                strategy="lazyOnload"
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA}`}
-            />
-            <Script id="ga-script" strategy="lazyOnload">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.GA}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-            </Script>
             <head />
             <body>
                 <Header />
