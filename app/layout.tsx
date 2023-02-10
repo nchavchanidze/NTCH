5fdb9f1488882ad1db39f505266e2c0b846d7895
nchavchanidze <nikolozchavchanidze@gmail.com>
2023-02-10 18:30:31 +0400

    Add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,33 @@
-import Header from '@/components/Header/Header'
+import Script from "next/script";
+import "@/styles/styles.scss";
+import Header from "@/components/Header/Header";
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>
-        <Header />
-        {children}
-        </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <Script
+                strategy="lazyOnload"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA}`}
+            />
+            <Script id="ga-script" strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.GA}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+            <head />
+            <body>
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
